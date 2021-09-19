46d4cc3bff0045c6b54fdfd0acdd8d0fe4bf7100
vasily <pozlevich12@gmail.com>
2021-09-20 01:33:08 +0300

    sasd

---
+++ src/main/java/com/table/TableApplication.java
@@ -0,0 +1,41 @@
+package com.table;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TableApplication extends WebSecurityConfigurerAdapter {
+
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TableApplication.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests(
+                a -> a.antMatchers("/", "/js/**", "/chart", "/css/**", "/error", "/webjars/**").permitAll().anyRequest().authenticated())
+                .exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .logout(l -> l.logoutSuccessUrl("/").permitAll()).oauth2Login();
+    }
+    
+}
